aa85ae1ebdf5affbd6c765e8e26c656331c1c261
darshan-1204 <dabhidarshan2004@gmail.com>
2023-01-18 09:39:05 +0530

    commit

--- app/src/main/java/com/example/examproject/Page3.java
+++ app/src/main/java/com/example/examproject/Page3.java
@@ -2,13 +2,30 @@ package com.example.examproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Page3 extends AppCompatActivity {
 
+    ImageView back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page3);
+
+        back = findViewById(R.id.back_arrow3);
+
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Page3.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
